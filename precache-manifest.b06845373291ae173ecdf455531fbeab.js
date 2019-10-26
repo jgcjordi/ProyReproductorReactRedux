409494d773e64e91fc9c69c0b5691cf72c623458
Jordi Gomis <jgc.jordi@gmail.com>
2019-10-26 21:31:02 +0200

    Updates

---
+++ precache-manifest.b06845373291ae173ecdf455531fbeab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d421b9d208ebf7d8cabc91d007d4f95a",
+    "url": "/ProyReproductorReactRedux/index.html"
+  },
+  {
+    "revision": "ee0a703c5b6bc8f5ed91",
+    "url": "/ProyReproductorReactRedux/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3dd106497ca6d59b2042",
+    "url": "/ProyReproductorReactRedux/static/js/2.562768cc.chunk.js"
+  },
+  {
+    "revision": "ee0a703c5b6bc8f5ed91",
+    "url": "/ProyReproductorReactRedux/static/js/main.e5031de0.chunk.js"
+  },
+  {
+    "revision": "a2a4564be3067ce5e69a",
+    "url": "/ProyReproductorReactRedux/static/js/runtime-main.76168a7d.js"
+  },
+  {
+    "revision": "fbb3aa476cd71398e35f360e927f9f30",
+    "url": "/ProyReproductorReactRedux/static/media/discDefault.fbb3aa47.jpg"
+  },
+  {
+    "revision": "3c41c5a7768b4d6edbe88e78e4ec51d5",
+    "url": "/ProyReproductorReactRedux/static/media/reproductor.3c41c5a7.jpg"
+  }
+]);
